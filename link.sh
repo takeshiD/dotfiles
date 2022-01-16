955c9c7602b5049e165a0ae0ad6c04c96ebbe2db
tkcd-px-X1 <goriponikeike55@gmail.com>
2022-01-16 19:06:44 +0900

    modified: link.sh

--- link.sh
+++ link.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-ln -sv ~/dotfiles/.vimrc ~/.vimrc
-ln -sv ~/dotfiles ~/.vim
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+RET=$(ln -svf $SCRIPT_DIR/.vimrc $HOME/.vimrc)
+echo "[Created SymLink]" $RET
+RET=$(ln -svfn $SCRIPT_DIR $HOME/.vim)
+echo "[Created SymLink]" $RET
 
+mkdir -p $HOME/.vim/.undo
+echo "[Created Directory]" $HOME/.vim/.undo
