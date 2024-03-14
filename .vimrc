9bcc1f0a95dc938fd9726a627f36ac06fd0bd71f
takeshiD <goriponikeike55@gmail.com>
2024-03-14 14:15:16 +0900

    add powerline-shell config

--- .vimrc
+++ .vimrc
@@ -131,9 +131,10 @@ smap <C-k>  <Plug>(neosnippet_expand_or_jump)
 xmap <C-k>  <Plug>(neosnippet_expand_target)
 smap <expr><TAB> neosnippet#expandable_or_jumpable() ?
             \ "\<Plug>(neosnippet_expand_or_jump)" : "\<TAB>"
-if has('conceal')
-    set conceallevel=2 concealcursor=niv
-endif
+"if has('conceal')
+"    set conceallevel=2 concealcursor=niv
+"endif
+
 let g:neosnippet#snippets_directory='~/.vim/bundle/neosnippet-snippets/snippets/,~/.vim/snippets'
 
 "------ Undo Persistent ------
--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 DEIN_DIR=$(cd $(dirname $0); pwd)
 
-RET=$(ln -svf $DEIN_DIR/.vimrc $HOME/.vimrc)
+RET=$(ln -svb $DEIN_DIR/.vimrc $HOME/.vimrc)
 echo "[Created SymLink]" $RET
 
-RET=$(ln -svfn $DEIN_DIR $HOME/.vim)
+RET=$(ln -svb $DEIN_DIR $HOME/.vim)
+echo "[Created SymLink]" $RET
+
+RET=$(ln -svb $DEIN_DIR/.bashrc $HOME/.bashrc)
+echo "[Created SymLink]" $RET
+
+RET=$(ln -svb $DEIN_DIR/.inputrc $HOME/.inputrc)
 echo "[Created SymLink]" $RET
 
 mkdir -p $HOME/.vim/.undo
 echo "[Created Directory]" $HOME/.vim/.undo
+
+mkdir -p $HOME/.config/powerline-shell
+echo "[Created Directory]" $HOME/.config/powerline-shell
+
+RET=$(ln -svb $DEIN_DIR/config/powerline-shell/config $HOME/.config/powerline-shell)
+echo "[Created SymLink]" $RET
