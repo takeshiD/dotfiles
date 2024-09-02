c9b9d81b6c533bdc9cf106431d69021147e1d20f
takeshid <goriponikeike55@gmail.com>
2024-09-02 15:47:34 +0900

    update

---
+++ config/nvim/lua/keymap.lua
@@ -0,0 +1,12 @@
+local keymap = vim.keymap
+keymap.set('n', '<ESC><ESC>', ':noh<Return>')
+keymap.set('n', 'j', 'gj')
+keymap.set('n', 'k', 'gk')
+keymap.set('n', 'gj', 'j')
+keymap.set('n', 'gk', 'k')
+keymap.set('n', '<Return><Return>', '<C-w>w')
+keymap.set('n', '<C-p>', ':bprev<Return>')
+keymap.set('n', '<C-n>', ':bnext<Return>')
+keymap.set('n', '<C-w><C-w>', ':bdelete %<Return>')
+keymap.set('n', 'L', '$')
+keymap.set('i', 'jj', '<ESC>')
