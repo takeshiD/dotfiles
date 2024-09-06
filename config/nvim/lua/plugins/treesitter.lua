d9a5c5750f4b3f61240c76b90d888d7fad1bd9eb
takeshid <goriponikeike55@gmail.com>
2024-09-06 16:40:10 +0900

    update

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "lua",
+                "vim",
+                "rust",
+                "markdown",
+                "markdown_inline",
+                "haskell",
+                "http",
+                "css",
+                "javascript",
+                "typescript",
+                "tsx",
+                "css",
+                "json",
+                "latex",
+                "make",
+                "python",
+                "scheme",
+                "yaml",
+                "toml"
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+            autotag = {
+                enable = true,
+            },
+        })
+    end
+}
