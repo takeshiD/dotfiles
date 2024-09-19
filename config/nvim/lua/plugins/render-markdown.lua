1f48a33223e566b38be5f3ae3c036c49386357e7
shinji.ishii <shinji.ishii@agc.com>
2024-09-19 15:52:44 +0900

    update

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,9 +1,11 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
+    lazy = false,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons"
     },
+    ft = {"markdown"},
     keys = {
         {"<leader>t", "<cmd>RenderMarkdown toggle<CR>", mode = {"n"}},
     },
@@ -14,12 +16,12 @@ return {
                 enabled = true,
                 position = "inline",
                 unchecked = {
-                    icon = "⬜ ",
-                    highlight = "RenderMarkdownUnchecked",
+                    icon = "󰄱 ",
+                    highlight = "HintSign",
                 },
                 checked = {
-                    icon = "✅ ",
-                    highlight = "RenderMarkdownChecked",
+                    icon = "󰄲 ",
+                    highlight = "OkSign",
                 }
             }
         })
