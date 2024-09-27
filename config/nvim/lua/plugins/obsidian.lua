43cc1ae927b655428a5126f3eeb18e88e311b718
shinji.ishii <shinji.ishii@agc.com>
2024-09-27 09:27:49 +0900

    update

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -3,11 +3,7 @@ return {
     version = "*",
     lazy = true,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     event = {
-        -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-        -- refer to `:h file-pattern` for more examples
         "BufReadPre ~/vaults/**/*.md",
         "BufNewFile ~/vaults/**/*.md",
     },
@@ -16,6 +12,9 @@ return {
         "hrsh7th/nvim-cmp",
         "nvim-telescope/telescope.nvim",
     },
+    init = function()
+        vim.opt.conceallevel = 1
+    end,
     opts = {
         workspaces = {
             {
@@ -25,6 +24,39 @@ return {
             {
                 name = "work",
                 path = "~/vaults/work",
+            },
+        },
+        daily_notes = {
+            folder = "daily",
+            date_format = "%Y-%m-%d",
+            default_tags = { "daily" },
+            template = nil,
+        },
+        ui = {
+            enable = true,
+        },
+        completion = {
+            nvim_cmp = true,
+            min_chars = 2,
+        },
+        mappings = {
+            ["<leader>of"] = {
+                action = function()
+                    return require("obsidian").util.gf_passthrough()
+                end,
+                opts = { noremap = false, expr = true, buffer = true },
+            },
+            ["<leader>oc"] = {
+                action = function()
+                    return require("obsidian").util.toggle_checkbox()
+                end,
+                opts = { buffer = true },
+            },
+            ["<CR>"] = {
+                action = function()
+                    return require("obsidian").util.smart_action()
+                end,
+                opts = { buffer = true, expr = true },
             },
         },
     },
