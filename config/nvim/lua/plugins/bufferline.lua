60cacaae8bb63cebd12cabb9ce31d3ee49f4b03e
takeshid <goriponikeike55@gmail.com>
2024-10-28 00:21:00 +0900

    update

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -1,14 +1,25 @@
+local colors = {
+    fg = vim.api.nvim_get_hl(0, { name = "IncSearch" }).fg,
+    bg = vim.api.nvim_get_hl(0, { name = "IncSearch" }).bg,
+}
+
 return {
     "akinsho/bufferline.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- lazy = false,
     event = "VeryLazy",
     opts = {
         options = {
             diagnostics = "nvim_lsp",
+            indicator = {
+                style = "underline",
+            },
+            separator_style = "slant",
         },
+        highlights = {
+        }
     },
-    keys = {
-        { "<Tab>",   "<cmd>BufferLineCycleNext<CR>", { silent = true, noremap = true, desc = "Buffer Next" } },
-        { "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { silent = true, noremap = true, desc = "Buffer Prev" } },
-    },
+    config = function(_, opts)
+        require("bufferline").setup(opts)
+    end
 }
