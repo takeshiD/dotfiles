878bff1766837e01ff2f61fc2594bc17ac3ff497
shinji.ishii <shinji.ishii@agc.com>
2024-11-05 15:27:41 +0900

    update

--- config/nvim/lua/plugins/avente.lua
+++ config/nvim/lua/plugins/avente.lua
@@ -1,8 +1,57 @@
-local exist_env = function(envname)
+---環境変数envnameが存在する場合はtrue, しない場合はfalseを返す
+---@param envname string
+---@return boolean
+local exist_envname = function(envname)
     return vim.fn.getenv(envname) ~= vim.Nil
 end
+
+---condがtrueの場合Tを返す condがfalseの場合Fを返す
+---@param cond boolean
+---@param T any
+---@param F any
+---@return any
 local ternary = function(cond, T, F)
     if cond then return T else return F end
+end
+
+---述語predがtrueとなるlistの最初の要素を返す
+---全ての要素がfalseの場合はnilを返す
+---@generic T
+---@param pred fun(item: T): boolean
+---@param list T[]
+---@return T | nil
+local member_if = function(pred, list)
+    for k, v in pairs(list) do
+        if pred(v) then
+            return k
+        end
+    end
+    return nil
+end
+
+---環境変数envnameが存在する場合は格納されている値を返す
+---存在しない場合はFを返す. Fはデフォルト値が空文字
+---@param envname string
+---@param F? string
+---@return string
+local getenv_if = function(envname, F)
+    F = F or ""
+    return ternary(exist_envname(envname), vim.fn.getenv(envname), F)
+end
+
+local copilot_path = vim.fn.stdpath("data") .. "/avante/github-copilot.json"
+
+---providersの中で最初に環境変数が設定されているproviderを返す
+---先頭に設定されているproviderが優先されるため自分で優先度を設定すること
+---@param providers string[]
+---@return string
+local get_provider = function(providers)
+    local provider = member_if(exist_envname, providers)
+    if provider == nil then
+        error("The provider for which the api-key is set cannot be obtained.")
+    else
+        return provider
+    end
 end
 
 return {
@@ -13,11 +62,25 @@ return {
     version = false, -- set this if you want to always pull the latest change
     opts = {
         debug = false,
-        provider = "claude",
+        -- provider = "claude",
         -- provider = "copilot",
         -- provider = "openai",
-        -- provider = "azure",
-        auto_suggestions_provider = "copilot",
+        provider = get_provider(
+            {
+                azure = "AZURE_OPENAI_API_KEY",
+                claude = "ANTHROPIC_API_KEY",
+                openai = "OPENAI_API_KEY",
+            }
+        ),
+        -- auto_suggestions_provider = "copilot",
+        -- auto_suggestions_provider = "claude",
+        auto_suggestions_provider = get_provider(
+            {
+                azure = "AZURE_OPENAI_API_KEY",
+                claude = "ANTHROPIC_API_KEY",
+                openai = "OPENAI_API_KEY",
+            }
+        ),
         behaviour = {
             auto_suggestions = true,
             auto_set_highlight_group = true,
@@ -40,11 +103,9 @@ return {
         },
         -- providers-setting
         claude = {
-            -- model = "claude-3-5-sonnet-20240620", -- $3/$15, maxtokens=8000
-            model = "claude-3-5-sonnet-20241022", -- $3/$15, maxtokens=8000
-            -- model = "claude-3-opus-20240229",  -- $15/$75
-            -- model = "claude-3-haiku-20240307", -- $0.25/1.25
-            -- model = "claude-3-5-haiku-20241022", -- $0.25/1.25k
+            model = "claude-3-5-sonnet-20241022", -- $3/$15, maxtokens=8192
+            -- model = "claude-3-5-haiku-20241022", -- $1/$5, maxtokens=8192
+            -- model = "claude-3-opus-20240229",  -- $15/$75, maxtokens=4096
             -- max_tokens = 4096,
             max_tokens = 8000,
         },
@@ -58,12 +119,12 @@ return {
             -- model = "gpt-4o-mini", -- $0.15/$0.60
             max_tokens = 4096,
         },
-        -- azure = {
-        --     endpoint = ternary(exist_env("AZURE_OPENAI_ENDPOINT_ADDR"), "http://" .. vim.fn.getenv("AZURE_OPENAI_ENDPOINT_ADDR"), ""),
-        --     deployment = ternary(exist_env("AZURE_OPENAI_DEPLOY"), vim.fn.getenv("AZURE_OPENAI_DEPLOY"), ""),
-        --     api_version = "2024-06-01",
-        --     max_tokens = 4096,
-        -- }
+        azure = {
+            endpoint = getenv_if("AZURE_OPENAI_ENDPOINT"),
+            deployment = getenv_if("AZURE_OPENAI_DEPLOY"),
+            api_version = "2024-06-01",
+            max_tokens = 4096,
+        },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- build = "make",
@@ -75,7 +136,7 @@ return {
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
         "nvim-tree/nvim-web-devicons",
-        "zbirenbaum/copilot.lua",  -- for providers='copilot'
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
         {
             -- support for image pasting
             "HakonHarnes/img-clip.nvim",
