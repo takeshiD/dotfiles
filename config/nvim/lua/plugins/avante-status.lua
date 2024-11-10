3705dc1f68a38dbbb9b21248968de51da9df2907
takeshid <goriponikeike55@gmail.com>
2024-11-11 00:17:06 +0900

    update

--- config/nvim/lua/plugins/avante-status.lua
+++ config/nvim/lua/plugins/avante-status.lua
@@ -1,10 +1,11 @@
 return {
     "takeshid/avante-status.nvim",
-    enabled = false,
-    event = "VeryLazy",
-    dir = vim.fn.stdpath("data") .. "/develop/avante-status.nvim",
-    fallback = true,
-    config = function()
-        require('avante-status').setup()
-    end
+    enabled = true,
+    lazy = false,
+    -- event = "VeryLazy",
+    -- dir = vim.fn.stdpath("data") .. "/develop/avante-status.nvim",
+    fallback = false,
+    -- config = function()
+    --     require('avante-status').setup()
+    -- end
 }
