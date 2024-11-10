b05c6deb0db2039dfc50e1ee1240fb2dbf6dd48e
takeshid <goriponikeike55@gmail.com>
2024-11-11 00:44:53 +0900

    updarte

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -11,3 +11,32 @@ vim.api.nvim_create_autocmd("LspAttach", {
         require("core.lsp")
     end
 })
+
+-- local view_group = vim.api.nvim_create_augroup('ViewSettings', { clear = true })
+--
+-- vim.api.nvim_create_autocmd('BufWritePost', {
+--     group = view_group,
+--     callback = function()
+--         local bufname = vim.fn.expand('%')
+--         local buftype = vim.bo.buftype
+--         if bufname ~= '' and buftype:match('nofile') == nil then
+--             vim.defer_fn(function()
+--                 vim.cmd('mkview')
+--             end, 100)
+--         end
+--     end
+-- })
+--
+-- vim.api.nvim_create_autocmd('BufRead', {
+--     group = view_group,
+--     callback = function()
+--         local bufname = vim.fn.expand('%')
+--         local buftype = vim.bo.buftype
+--         if bufname ~= '' and buftype:match('nofile') == nil then
+--             vim.defer_fn(function()
+--                 vim.cmd('silent! loadview')
+--             end, 100)
+--         end
+--     end
+-- })
+-- vim.opt.viewoptions:remove('options')
