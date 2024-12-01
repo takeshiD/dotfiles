437249ed0acb01f0a39904588ae1ef55820d4247
takeshid <goriponikeike55@gmail.com>
2024-12-01 22:17:03 +0900

    update

--- config/nvim/lua/plugins/git-blame.lua
+++ config/nvim/lua/plugins/git-blame.lua
@@ -5,7 +5,7 @@ return {
         enabled = true,
         message_template = " <author> '<summary>' : <date>",
         date_format = "%r (%Y-%m-%d %H:%M:%S)",
-        messge_when_not_commited = " Not Commited Yet",
+        message_when_not_commited = " Not Commited Yet",
         virtual_text_column = 60,
         display_virtual_text = 1,
     },
