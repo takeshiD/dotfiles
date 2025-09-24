6fa2284f8d498ca3609874466d261cda97b2ba27
takeshid <goriponikeike55@gmail.com>
2025-09-24 16:12:00 +0900

    update diffview

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -1,19 +1,33 @@
 return {
-    'sindrets/diffview.nvim',
-    event = "VeryLazy",
-    enabled = true,
-    keys = {
-        { mode = {"n"}, "<leader>dh", "<cmd>DiffviewFileHistory %<CR>"},
-        { mode = {"n"}, "<leader>df", function()
-            if next(require("diffview.lib").views) == nil then
-                vim.cmd("DiffviewOpen")
-            else
-                vim.cmd("DiffviewClose")
-            end
-        end
-        },
-    },
-    config = function()
-        require('diffview').setup()
-    end
+	"sindrets/diffview.nvim",
+	event = "VeryLazy",
+	enabled = true,
+	keys = {
+		{ mode = { "n" }, "<leader>dh", "<cmd>DiffviewFileHistory %<CR>", desc = "Unstaged FileHistory" },
+		{
+			mode = { "n" },
+			"<leader>df",
+			function()
+				if next(require("diffview.lib").views) == nil then
+					vim.cmd("DiffviewOpen HEAD~1")
+				else
+					vim.cmd("DiffviewClose")
+				end
+			end,
+			desc = "Diff Previous Commit to HEAD",
+		},
+	},
+	config = function()
+		local actions = require("diffview.actions")
+		require("diffview").setup({
+			keymaps = {
+				view = {
+					{ "n", "<C-e>", actions.toggle_files, { desc = "Toggle FilePanel" } },
+				},
+				file_panel = {
+					{ "n", "<C-e>", actions.toggle_files, { desc = "Toggle FilePanel" } },
+				},
+			},
+		})
+	end,
 }
--- config/nvim/lua/plugins/windows-nvim.lua
+++ config/nvim/lua/plugins/windows-nvim.lua
@@ -1,29 +1,32 @@
 return {
 	"anuvyklack/windows.nvim",
-    enabled = true,
-    lazy = false,
+	enabled = true,
+	lazy = false,
 	dependencies = {
 		"anuvyklack/middleclass",
 		"anuvyklack/animation.nvim",
 	},
-    keys = {
-        {"<C-w>z", "<cmd>WindowsMaximize<cr>", mode = {"n"}, desc = "Window Maximize"},
-        {"<C-w>_", "<cmd>WindowsMaximizeVertically<cr>", mode = {"n"}, desc = "Window Maximize Vertical"},
-        {"<C-w>|", "<cmd>WindowsMaximizeHorizontally<cr>", mode = {"n"}, desc = "Window Maximize Horizontal"},
-        {"<C-w>=", "<cmd>WindowsEqualize<cr>", mode = {"n"}, desc = "Window Equalize"},
-        {"<C-w>t", "<cmd>WindowsToggleAutowidth<cr>", mode = {"n"}, desc = "Window Toggle Autowidth"},
-    },
+	keys = {
+		{ "<C-w>z", "<cmd>WindowsMaximize<cr>", mode = { "n" }, desc = "Window Maximize" },
+		{ "<C-w>_", "<cmd>WindowsMaximizeVertically<cr>", mode = { "n" }, desc = "Window Maximize Vertical" },
+		{ "<C-w>|", "<cmd>WindowsMaximizeHorizontally<cr>", mode = { "n" }, desc = "Window Maximize Horizontal" },
+		{ "<C-w>=", "<cmd>WindowsEqualize<cr>", mode = { "n" }, desc = "Window Equalize" },
+		{ "<C-w>t", "<cmd>WindowsToggleAutowidth<cr>", mode = { "n" }, desc = "Window Toggle Autowidth" },
+	},
 	config = function()
-		vim.o.winwidth = 10     -- Minimum current window width
-        vim.o.winminwidth = 10  -- Minimum window width other than current window
+		vim.o.winwidth = 10 -- Minimum current window width
+		vim.o.winminwidth = 10 -- Minimum window width other than current window
 		vim.o.equalalways = false
 		require("windows").setup({
-            animation = {
-                enable = true,
-                duration = 200,
-                fps = 30,
-                easing = "in_out_quad"
-            }
-        })
+			animation = {
+				enable = true,
+				duration = 200,
+				fps = 30,
+				easing = "in_out_quad",
+			},
+			ignore = {
+				filetype = { "neo-tree", "DiffViewFiles" },
+			},
+		})
 	end,
 }
