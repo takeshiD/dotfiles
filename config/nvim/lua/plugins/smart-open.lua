c04c5b92cc42fdea2e41dc741b2ad4ebc94d951f
takeshid <goriponikeike55@gmail.com>
2025-10-20 08:58:00 +0900

    add smart-open key-map

--- config/nvim/lua/plugins/smart-open.lua
+++ config/nvim/lua/plugins/smart-open.lua
@@ -1,11 +1,14 @@
 return {
 	"danielfalk/smart-open.nvim",
 	branch = "0.2.x",
-	config = function()
-		require("telescope").load_extension("smart_open")
-	end,
 	dependencies = {
 		"kkharji/sqlite.lua",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
+	config = function()
+		require("telescope").load_extension("smart_open")
+		vim.keymap.set("n", "<leader><leader>", function()
+			require("telescope").extensions.smart_open.smart_open()
+		end, { noremap = true, silent = true })
+	end,
 }
