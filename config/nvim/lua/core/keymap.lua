7c643c0652fe84856469eaa48903ba74f7175b83
takeshid <goriponikeike55@gmail.com>
2025-10-20 08:39:02 +0900

    add large up/down

--- config/nvim/lua/core/keymap.lua
+++ config/nvim/lua/core/keymap.lua
@@ -4,19 +4,27 @@ vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 0
 
 keymap.set("n", "<ESC><ESC>", ":noh<Return>")
-keymap.set("n", "j", "gj", { desc = "Down" })
-keymap.set("n", "k", "gk", { desc = "Up" })
-keymap.set("n", "gj", "j", { desc = "Left" })
-keymap.set("n", "gk", "k", { desc = "Right" })
 keymap.set("n", "<Return><Return>", "<C-w>w", { desc = "BufferCyclic" })
 keymap.set("n", "<C-p>", ":bprev<Return>")
 keymap.set("n", "<C-n>", ":bnext<Return>")
 keymap.set("n", "<C-w><C-w>", ":bdelete %<Return>")
+keymap.set("i", "jj", "<ESC>")
+
+--############# Motion      ###############
+keymap.set("n", "j", "gj", { desc = "Down" })
+keymap.set("n", "k", "gk", { desc = "Up" })
+keymap.set("n", "gj", "j", { desc = "Left" })
+keymap.set("n", "gk", "k", { desc = "Right" })
+
 keymap.set("n", "L", "$")
 keymap.set("n", "H", "^")
 keymap.set("v", "L", "$")
 keymap.set("v", "H", "^")
-keymap.set("i", "jj", "<ESC>")
+
+keymap.set("n", "J", "5gj", { desc = "Large Down" })
+keymap.set("n", "K", "5gk", { desc = "Large Up" })
+keymap.set("v", "J", "5gj", { desc = "Large Down" })
+keymap.set("v", "K", "5gk", { desc = "Large Up" })
 
 --############# US-keyboard ###############
 keymap.set("n", ";", ":")
