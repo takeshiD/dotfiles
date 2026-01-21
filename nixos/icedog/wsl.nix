5006f63ebb8770728cf5b4af15945dc7c3c70e7a
tkcd <goriponikeike55@gmail.com>
2026-01-21 22:49:17 +0900

    update

--- nixos/icedog/wsl.nix
+++ nixos/icedog/wsl.nix
@@ -18,7 +18,7 @@
     substituters = [
       "https://cache.numtide.com"
     ];
-    trusted-substituters = [];
+    trusted-substituters = [ ];
     trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
@@ -35,13 +35,8 @@
   ];
 
   wsl.enable = true;
-  wsl.defaultUser = "nixos";
-  wsl.wslConf.interop.enabled = false;
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  wsl.defaultUser = "tkcd";
+  wsl.interop.enabled = false;
+  wsl.wslConf.hostname = "icedog";
   system.stateVersion = "25.11"; # Did you read the comment?
 }
