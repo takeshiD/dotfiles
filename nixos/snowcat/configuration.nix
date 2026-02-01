b79e81e92c45238c1d926e5a97097472a07470ce
tkcd <goriponikeike55@gmail.com>
2026-02-01 13:36:29 +0900

    chore: add trusted ros

--- nixos/snowcat/configuration.nix
+++ nixos/snowcat/configuration.nix
@@ -8,11 +8,16 @@
     substituters = [
       "https://hyprland.cachix.org"
       "https://cache.numtide.com"
+      "https://ros.cachix.org"
     ];
-    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [
+      "https://hyprland.cachix.org"
+      "https://ros.cachix.org"
+    ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
     ];
     experimental-features = [
       "nix-command"
