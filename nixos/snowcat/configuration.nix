9638170013ef4a0ba7a7b3d94d4d3e4a2cad5a25
tkcd <goriponikeike55@gmail.com>
2026-02-08 09:43:33 +0900

    chore: update

--- nixos/snowcat/configuration.nix
+++ nixos/snowcat/configuration.nix
@@ -140,20 +140,19 @@
   # Power Management and Behaviour closing the lid. #
   # > see https://wiki.nixos.org/wiki/Laptop        #
   ###################################################
-  boot.kernelParams = [ "mem_sleep_default=deep" ];
   services.logind.settings.Login = {
     # "ignore": no action
     # "poweroff": shutdown os and turn off
     # "halt": shutdown os and NOT turn off
     # "reboot": reboot os and NOT turn off
     # "kexec": ?
-    # "suspend": NOT turn off RAM, turn off CPU
+    # "suspend": NOT turn off RAM, turn off CPUoption
     # "hibernate": Save RAM data to swap partition, turn off
     # "hybrid-sleep": first suspend,
     # "suspend-then-hibernate": closing the lid and suspend, spent few minutes and hybernate
     # "lock": screen lock only
     # Battely supply
-    HandleLidSwitch = "hibernate";
+    HandleLidSwitch = "suspend-then-hibernate";
     # Power is connected
     HandleLidSwitchExternalPower = "suspend";
     # Another screen is connected(cram shell)
