57851e64d470ebb8a4d8c7d5da085173d261d5ea
tkcd <goriponikeike55@gmail.com>
2026-02-24 23:34:32 +0900

    chore: update

--- nixos/snowcat/configuration.nix
+++ nixos/snowcat/configuration.nix
@@ -230,6 +230,7 @@
       };
     };
   };
+  programs.nix-ld.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
