396a54f06d1bb63663bc45e6b3ab3d1507dafe43
tkcd <goriponikeike55@gmail.com>
2026-03-03 14:56:15 +0900

    chore: add samoyed

---
+++ nixos/samoyed/wsl.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  nix.settings = {
+    trusted-users = [
+      "root"
+      "tkcd"
+    ];
+    substituters = [
+      "https://cache.numtide.com"
+    ];
+    trusted-substituters = [ ];
+    trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    wget
+    git
+    home-manager
+    docker
+  ];
+  programs.nix-ld.enable = true;
+  security.pki.certificates = [
+    (builtins.readFile /home/tkcd/cert/zscaler.crt)
+    (builtins.readFile /home/tkcd/cert/agcglobal.cer)
+  ];
+  # networking.enableIPv6 = false;
+  virtualisation.docker = {
+    enable = true;
+  };
+  users.users.tkcd = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
+  };
+  time.timeZone = "Asia/Tokyo";
+  wsl.enable = true;
+  wsl.defaultUser = "tkcd";
+  wsl.wslConf.interop = {
+    enabled = true;
+    appendWindowsPath = false;
+  };
+  wsl.wslConf.network.hostname = "samoyed";
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
