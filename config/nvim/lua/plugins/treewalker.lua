950fea311f8e3c862e0ffe1c4c15b2959a647534
tkcd <goriponikeike55@gmail.com>
2026-03-10 10:13:00 +0900

    update

--- config/nvim/lua/plugins/treewalker.lua
+++ config/nvim/lua/plugins/treewalker.lua
@@ -3,8 +3,8 @@ return {
 	keys = {
 		{ "<C-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" }, desc = "Treewaler Right" },
 		{ "<C-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" }, desc = "Treewalker Left" },
-		{ "<C-[>", "<cmd>Treewalker SwapUp<cr>", mode = { "n" }, desc = "Treewalker SwapUp" },
-		{ "<C-]>", "<cmd>Treewalker SwapDown<cr>", mode = { "n" }, desc = "Treewalker SwapDown" },
+		-- { "<C-[>", "<cmd>Treewalker SwapUp<cr>", mode = { "n" }, desc = "Treewalker SwapUp" },
+		-- { "<C-]>", "<cmd>Treewalker SwapDown<cr>", mode = { "n" }, desc = "Treewalker SwapDown" },
 		-- { "<C-,>", "<cmd>Treewalker SwapLeft<cr>", mode = { "n" }, desc = "Treewalker SwapLeft" },
 		-- { "<C-.>", "<cmd>Treewalker SwapRight<cr>", mode = { "n" }, desc = "Treewalker SwapRight" },
 	},
